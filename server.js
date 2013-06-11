dc0f05e65a68dd32469861e78221d19356bc0ed3
Rick <torres.rick@gmail.com>
2013-06-11 19:43:15 +0200

    Create files
    Now it creates crops with the resize library (easyimage) but  looks
    like I don't assign the correct permissions so I can read the image.

--- server.js
+++ server.js
@@ -19,36 +19,63 @@ var mimeTypes = {
 
 http.createServer(function (request, response) {
 
-	// need to normalize   path.normalize ?
+	var url = path.normalize(request.url);
 	
-	if(request.url != '/favicon.ico'){
+	if(url != '/favicon.ico'){
 	
-		var urlString = request.url;
+		var urlString = url;
 		
 		var new_array = urlString.split('/');
 		
 		var user_width = new_array[1];
 		var user_height = new_array[2];
-				
-		console.log(new_array);
-	}
-	
-
-	 var img = fs.readFileSync('./cat.jpg');
-	 
-	 easyimg.info('./cat.jpg', function(err,stdout,stderr) {
-		  console.log('Next line should be unsupported error');
-		  console.log(err);
-	});
-	 
-	 easyimg.resize({src:'./cat.jpg', dst:'./resize.jpg', width:100, height:100}, function(err, image) {
+			
+			
+		 
+		easyimg.info('./cat.jpg', function(err,stdout,stderr) {
+			  console.log('Next line should be unsupported error');
+			  console.log(err);
+		});
 		
-		console.log('Resized');
-		console.log(image);
-	});	 
+		var original_img = './cat.jpg';
+		 
+		/* easyimg.resize({src:original_img, dst:'./resize.jpg', width:user_width, height:user_height}, function(err, image) {
+			
+			console.log('Resized');
+			console.log(image);
+		});	*/
+		
+		
+		var rand_number = Math.floor((Math.random()*500)+1);
+		
+		var dest_img = './cache/resize-' + rand_number + '.jpg';
+//		var dest_img = './resize.jpg';
+		fs.open(dest_img, 'w+', '0777');
 
-     response.writeHead(200, {'Content-Type': 'image/gif' });
-     response.end(img, 'binary');
+		easyimg.rescrop(
+			{
+				src:original_img, dst:dest_img,
+				width:user_width, height:user_height,
+				cropwidth:user_width, cropheight:user_height,
+				x:0, y:0,
+				fill: true
+			},
+			function(err, image) {
+				if (err) throw err;
+				console.log('Resized and cropped');
+				console.log(image);
+			}
+		);		
+		
+		 var img = fs.readFileSync(dest_img);
+		
+	     response.writeHead(200, {'Content-Type': 'image/gif' });
+	     
+	      fs.unlink(dest_img);
+	     
+	     response.end(img, 'binary');   
+	     
+	 }
 
       
 }).listen(8080);
